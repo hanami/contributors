21b9851b12b567f20ae5d07eed186e4a8b9dfe41
Anton Davydov <antondavydov.o@gmail.com>
2017-03-23 02:23:24 +0300

    Display project name in show page

--- apps/web/views/contributors/show.rb
+++ apps/web/views/contributors/show.rb
@@ -1,5 +1,10 @@
 module Web::Views::Contributors
   class Show
     include Web::View
+
+    def project_name(commit)
+      matcher = commit.url.match(%r{github.com/hanami/([\w.]+)/})
+      matcher && matcher[1]
+    end
   end
 end
--- spec/web/views/contributors/show_spec.rb
+++ spec/web/views/contributors/show_spec.rb
@@ -6,4 +6,17 @@ RSpec.describe Web::Views::Contributors::Show do
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
 
+  describe '#project_name' do
+    context 'when url contain project' do
+      let(:commit) { Commit.new(url: 'github.com/hanami/hanami/') }
+
+      it { expect(view.project_name(commit)).to eq 'hanami' }
+    end
+
+    context 'when url not contain project' do
+      let(:commit) { Commit.new(url: '') }
+
+      it { expect(view.project_name(commit)).to eq nil }
+    end
+  end
 end
