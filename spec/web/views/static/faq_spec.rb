7a03a780b06df9853ed1738a87f92fa574610206
Anton Davydov <antondavydov.o@gmail.com>
2017-03-27 01:39:03 +0300

    Delete unnecessary test

--- spec/web/views/static/faq_spec.rb
+++ spec/web/views/static/faq_spec.rb
@@ -5,11 +5,4 @@ RSpec.describe Web::Views::Static::Faq do
   let(:template)  { Hanami::View::Template.new('apps/web/templates/static/faq.html.slim') }
   let(:view)      { described_class.new(template, exposures) }
   let(:rendered)  { view.render }
-
-  it 'exposes #foo' do
-    pending 'This is an auto-generated test. Edit it and add your own tests.'
-
-    # Example
-    expect(view.foo).to eq exposures.fetch(:foo)
-  end
 end
