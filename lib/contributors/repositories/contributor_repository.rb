242a35c9873faf777e3041051a1ba691d8d5c2b9
Anton Davydov <antondavydov.o@gmail.com>
2017-03-29 01:18:55 +0300

    Create method for get contributors in commit create data range

--- lib/contributors/repositories/contributor_repository.rb
+++ lib/contributors/repositories/contributor_repository.rb
@@ -4,8 +4,14 @@ class ContributorRepository < Hanami::Repository
     has_many :projects
   end
 
-  def all_with_commits
-    aggregate(:commits).as(Contributor).to_a.sort_by { |c| c.commits.count }.reverse!
+  def all_with_commits(range = nil)
+    query = if range
+      aggregate(:commits).contributors.join(commits).where(commits[:created_at].qualified => range).as(Contributor)
+    else
+      aggregate(:commits).as(Contributor)
+    end
+
+    query.to_a.sort_by { |c| c.commits.count }.reverse!
   end
 
   def find_by_github(github)
--- spec/contributors/repositories/contributor_repository_spec.rb
+++ spec/contributors/repositories/contributor_repository_spec.rb
@@ -2,13 +2,14 @@ RSpec.describe ContributorRepository do
   let(:repo) { ContributorRepository.new }
   let(:contributor) { repo.create(github: 'davydovanton') }
 
+  let(:commit_repo) { CommitRepository.new }
+
+  after { repo.clear }
+
   describe '#find_by_github' do
-    let(:commit_repo) { CommitRepository.new }
-
-    after { repo.clear }
-
     context 'when contributor has commits' do
       after { commit_repo.clear }
+
       it { expect(repo.find_by_github(contributor.github)).to be_a Contributor }
       it { expect(repo.find_by_github(contributor.github).github).to eq contributor.github }
     end
@@ -16,5 +17,22 @@ RSpec.describe ContributorRepository do
     context 'when contributor does not have commits' do
       it { expect(repo.find_by_github('github')).to eq nil }
     end
+  end
+
+  describe '#all_with_commits' do
+    before do
+      other_commiter = repo.create(github: 'test')
+
+      commit_repo.create(contributor_id: contributor.id, created_at: Time.now - 60 * 60 * 22)
+      commit_repo.create(contributor_id: other_commiter.id, created_at: Time.now - 60 * 60 * 24 * 5)
+    end
+
+    after { commit_repo.clear }
+
+    let(:range) { (Time.now - 60 * 60 * 24)..Time.now }
+
+    it { expect(repo.all_with_commits(range).count).to eq 1 }
+    it { expect(repo.all_with_commits(range).last.github).to eq 'davydovanton' }
+    it { expect(repo.all_with_commits(range).last.commits.count).to eq 1 }
   end
 end
