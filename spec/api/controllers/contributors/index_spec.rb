6450c8a2fff45dc2c9408392cfc12c32f2210622
Anton Davydov <antondavydov.o@gmail.com>
2017-04-17 21:52:47 +0300

    Update specs for api contributors#index

--- spec/api/controllers/contributors/index_spec.rb
+++ spec/api/controllers/contributors/index_spec.rb
@@ -3,16 +3,35 @@ require_relative '../../../../apps/api/controllers/contributors/index'
 RSpec.describe Api::Controllers::Contributors::Index do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
-
-  it { expect(action.call(params)).to be_success }
+  let(:repo)   { ContributorRepository.new }
 
   it { expect(action.call(params)).to be_success }
 
   context 'when db empty' do
-    before { action.call(params) }
-
     let(:json) { JSON.parse(action.call(params)[2][0]) }
 
     it { expect(json).to eq('count' => 0, 'data' => []) }
+  end
+
+  context 'when db has some commits' do
+    before do
+      contributor = repo.create(github: "davydovanton")
+
+      CommitRepository.new.create(
+        contributor_id: contributor.id,
+        sha: "d244",
+        url: "github.com",
+        title: "test"
+      )
+    end
+
+    after { repo.clear }
+
+    let(:json) { JSON.parse(action.call(params)[2][0]) }
+
+    it 'contains contributors information' do
+      expect(json['count']).to eq 1
+      expect(json['data'].count).to eq 1
+    end
   end
 end
