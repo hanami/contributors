68b8bc1e79ac97bb6a74e49523969cebc25ef5fd
Anton Davydov <antondavydov.o@gmail.com>
2017-04-21 12:45:30 +0300

    Try to use raw content method

--- apps/web/views/contributors/index.rb
+++ apps/web/views/contributors/index.rb
@@ -2,6 +2,14 @@ module Web::Views::Contributors
   class Index
     include Web::View
 
+    def raw_content
+      raw '<h1>Hello World!</h1>'
+    end
+
+    def h_content
+      h "<script>alert('xss')</script>"
+    end
+
     def link_to_github(contributor)
       link_to contributor.github, "/contributors/#{contributor.github}"
     end
