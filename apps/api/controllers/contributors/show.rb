79a2a64d4065f6e24aac02c7bbc10ed9ff55b45f
Anton Davydov <antondavydov.o@gmail.com>
2017-04-23 12:44:40 +0300

    Add simple error handler for api contributors#show

--- apps/api/controllers/contributors/show.rb
+++ apps/api/controllers/contributors/show.rb
@@ -3,6 +3,18 @@ module Api::Controllers::Contributors
     include Api::Action
 
     def call(params)
+      contributor = ContributorRepository.new.find_by_github(params[:id])
+      if contributor
+        send_json(status: :ok)
+      else
+        send_json(status: :error, message: 'contributor not found')
+      end
+    end
+
+    private
+
+    def repo
+      @repo ||= ContributorRepository.new
     end
   end
 end
--- spec/api/controllers/contributors/show_spec.rb
+++ spec/api/controllers/contributors/show_spec.rb
@@ -2,10 +2,19 @@ require_relative '../../../../apps/api/controllers/contributors/show'
 
 RSpec.describe Api::Controllers::Contributors::Show do
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { { id: contributor.github } }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  let(:repo) { ContributorRepository.new }
+  let(:contributor) { repo.create(github: 'davydovanton') }
+
+  it { expect(action.call(params)).to be_success }
+
+  after { repo.clear }
+
+  context 'when db empty' do
+    let(:params) { { id: 'empty' } }
+
+    it { expect(action.call(params)).to be_success }
+    it { expect(action.call(params)[2].first).to match('"status":"error","message":"contributor not found"') }
   end
 end
