c59ea4c2f3f19ea40036f1cf58d21947690cc319
Anton Davydov <antondavydov.o@gmail.com>
2017-07-26 19:22:49 +0300

    Add new field for all contributors

--- lib/graphql/schema.rb
+++ lib/graphql/schema.rb
@@ -4,7 +4,21 @@ module Graphql
   end
 end
 
-# query = <<-QUERY
+# query_all = <<-QUERY
+# {
+#   contributors {
+#     id
+#     github
+#     full_name
+#     avatar_url
+#   }
+# }
+# QUERY
+#
+# Graphql::Schema.execute(query_all)
+# # => [{"data"=>{"contributor"=>{"id"=>"101", "github"=>"jodosha", "full_name"=>nil, "avatar_url"=>"https://avatars2.githubusercontent.com/u/5089?v=3"}}}, ...]
+
+# query_first = <<-QUERY
 # {
 #   contributor(id: "101") {
 #     id
--- lib/graphql/types/query.rb
+++ lib/graphql/types/query.rb
@@ -4,11 +4,19 @@ module Graphql
       name "Query"
       description "The query root of this schema"
 
-      field :contributor do
-        type Contributor
+      field :contributor, Contributor do
         argument :id, !types.ID
         description "Find a Contributor by ID"
-        resolve ->(obj, args, ctx) { ContributorRepository.new.find(args['id']) }
+        resolve ->(obj, args, ctx) do
+          ContributorRepository.new.find(args['id'])
+        end
+      end
+
+      field :contributors, types[Contributor] do
+        description "Get all Contributors"
+        resolve ->(obj, args, ctx) do
+          ContributorRepository.new.all
+        end
       end
     end
   end
