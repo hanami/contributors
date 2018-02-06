c082d6fe6cf81a4e822ea2117e1744be671718b2
Nicolas Cavigneaux <nico@bounga.org>
2018-02-06 23:19:27 +0100

    Add commits count on user contributions page

--- apps/web/controllers/contributors/show.rb
+++ apps/web/controllers/contributors/show.rb
@@ -3,11 +3,14 @@ module Web::Controllers::Contributors
     include Web::Action
     include Hanami::Pagination::Action
 
-    expose :contributor, :commits
+    expose :contributor, :commits, :commits_count
 
     def call(params)
       @contributor = ContributorRepository.new.find_by_github(params[:id])
-      @commits = all_for_page(CommitRepository.new.all_for_contributor(@contributor.id))
+      commits = CommitRepository.new.all_for_contributor(@contributor.id)
+
+      @commits_count = commits.to_a.size
+      @commits = all_for_page(commits)
     end
   end
 end
