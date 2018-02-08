8cbed2db5824bb552d9294cff61f306f43e7ce78
Anton Davydov <antondavydov.o@gmail.com>
2018-02-08 14:37:48 +0300

    Replace admin access constants to initialize file

--- apps/admin/application.rb
+++ apps/admin/application.rb
@@ -254,13 +254,6 @@ module Admin
       # FRAMEWORKS
       #
 
-      # Configure the code that will yield each time Admin::Action is included
-      # This is useful for sharing common functionality
-      #
-      # See: http://www.rubydoc.info/gems/hanami-controller#Configuration
-      ADMIN_USERNAME = ENV.fetch('ADMIN_USERNAME')
-      ADMIN_PASSWORD = ENV.fetch('ADMIN_PASSWORD')
-
       controller.prepare do
         # include MyAuthentication # included in all the actions
         # before :authenticate!    # run an authentication before callback
--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,5 +1,12 @@
 require 'dry/system/container'
 
+# Configure the code that will yield each time Admin::Action is included
+# This is useful for sharing common functionality
+#
+# See: http://www.rubydoc.info/gems/hanami-controller#Configuration
+ADMIN_USERNAME = ENV.fetch('ADMIN_USERNAME')
+ADMIN_PASSWORD = ENV.fetch('ADMIN_PASSWORD')
+
 class Application < Dry::System::Container
   configure
 end
