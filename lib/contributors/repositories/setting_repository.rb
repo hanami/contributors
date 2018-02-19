fce43c504ef673927131b8c90cde29126497006b
Anton Davydov <antondavydov.o@gmail.com>
2018-02-19 15:21:01 +0300

    Always returns settings entity from repository

--- lib/contributors/repositories/setting_repository.rb
+++ lib/contributors/repositories/setting_repository.rb
@@ -1,6 +1,8 @@
 class SettingRepository < Hanami::Repository
-
   def for_display
-    settings.select(:title).last
+    settings
+      .order{ created_at.desc }
+      .limit(1).map_to(Setting)
+      .one || Setting.new
   end
 end
--- spec/admin/controllers/settings/index_spec.rb
+++ spec/admin/controllers/settings/index_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe Admin::Controllers::Settings::Index, type: :action do
   let(:action) { described_class.new }
   let(:params) { Hash[] }
-
   let(:settings_repo) { SettingRepository.new }
+
+  after { settings_repo.clear }
 
   it { expect(action.call(params)).to be_success }
 
@@ -11,7 +12,8 @@ RSpec.describe Admin::Controllers::Settings::Index, type: :action do
       context 'when db empty' do
         before { action.call(params) }
 
-        it { expect(action.settings.to_a).to eq [] }
+        it { expect(action.settings).to be_a Setting }
+        it { expect(action.settings.title).to eq nil }
       end
 
       context 'when db has settings' do
@@ -20,9 +22,8 @@ RSpec.describe Admin::Controllers::Settings::Index, type: :action do
           action.call(params)
         end
 
-        after { settings_repo.clear }
-
-        it { expect(action.settings).to eq({title: 'Hanami'}) }
+        it { expect(action.settings).to be_a Setting }
+        it { expect(action.settings.title).to eq 'Hanami' }
       end
     end
   end
